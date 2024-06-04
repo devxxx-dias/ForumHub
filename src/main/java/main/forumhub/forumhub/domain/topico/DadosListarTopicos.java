eebec1aa9af665d550d9e9366be69a99f12c225a
Maurício Dias <devxxx.mauricio@gmail.com>
2024-06-04 16:18:12 -0300

    Entrega de todos os Endpoints

--- src/main/java/main/forumhub/forumhub/domain/topico/DadosListarTopicos.java
+++ src/main/java/main/forumhub/forumhub/domain/topico/DadosListarTopicos.java
@@ -1,19 +1,25 @@
 package main.forumhub.forumhub.domain.topico;
 
 import main.forumhub.forumhub.domain.curso.Curso;
+import main.forumhub.forumhub.domain.resposta.DadosAtualizacaoResposta;
+import main.forumhub.forumhub.domain.resposta.DadosListarRespostas;
 import main.forumhub.forumhub.domain.resposta.Resposta;
 import main.forumhub.forumhub.domain.usuario.Usuario;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public record DadosListarTopicos(
         Long id,
         String titulo,
         String mensagem,
         LocalDateTime now,
-        EstadoDoTopico estadoDoTopico
+        EstadoDoTopico estadoDoTopico,
+       List<DadosListarRespostas> respostas
 ) {
     public DadosListarTopicos(Topico topico) {
-        this(topico.getId(), topico.getTitulo(), topico.getMensagem(), topico.getDataCriacao(), topico.getEstadoDoTopico());
+        this(topico.getId(), topico.getTitulo(), topico.getMensagem(), topico.getDataCriacao(), topico.getEstadoDoTopico(),topico.getResposta().stream().map(DadosListarRespostas::new).collect(Collectors.toUnmodifiableList()));
     }
 }
